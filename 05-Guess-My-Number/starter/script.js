e8cf9d6de19ac5741c07a6d5baae1d072dad474c
Julio Aguilera Jr <julioaguilera337@gmail.com>
2024-02-13 20:16:15 -0600

    added highscore feature.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,8 +11,9 @@
 // console.log(document.querySelector(".guess").value);
 
 let secretNumber = Math.trunc(Math.random() * 3) + 1;
-
 let score = 20;
+let highScore = 0;
+
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -30,6 +31,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if(score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
 
     //Usr selects a higher Num than the guess
   } else if (guess > secretNumber) {
